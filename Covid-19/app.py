9d9f013a75a8f44c2df2e69ee0d393c7d4c08450
rit-hub <72191249+rit-hub@users.noreply.github.com>
2021-06-14 11:33:57 +0530

    api added

--- Covid-19/app.py
+++ Covid-19/app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for
+from api import getApi
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-    
+@app.route('/',methods = ['POST', 'GET'])
+def getCountryName():
+   if request.method == 'POST':
+      user = request.form['input_name']
+      print(user)
+      A = getApi.Api()
+      user_data=A.get_report(user=user)
+      print(user_data)
+      return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
